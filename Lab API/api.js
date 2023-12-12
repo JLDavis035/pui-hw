0209ca100ad2783cda5cddb7d6bf73ad50b4891e
JLDavis035 <143553598+JLDavis035@users.noreply.github.com>
2023-12-12 01:09:16 -0500

    Portfolio Update

--- Lab API/api.js
+++
@@ -1,9 +0,0 @@
-window.onSpotifyIframeApiReady = (IFrameAPI) => {
-    const element = document.getElementById('embed-iframe');
-    const options = {
-    uri: 'spotify:episode:6I3ZzCxRhRkNqnQNo8AZPV'
-    };
-    const callback = (EmbedController) => {};
-    IFrameAPI.createController(element, options, callback);
-    };
-    
